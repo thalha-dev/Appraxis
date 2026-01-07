35fff849a29135caea19bc060da6f477e6830bb2
thalha-dev <dev.thalha@gmail.com>
2026-01-07 06:53:43 +0530

    initial setup

---
+++ backend/src/main/java/dev/thalha/appraxis/AppraxisApplication.java
@@ -0,0 +1,13 @@
+package dev.thalha.appraxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppraxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppraxisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/thalha/appraxis/AppraxisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thalha.appraxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppraxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
